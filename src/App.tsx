54f29d03aedd531bda45d4e1a64506fedc72291e
mattmuroya <matt.muroya@gmail.com>
2022-06-25 20:07:40 -1000

    implement basic md preview functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
+import { useState } from "react";
+import { marked } from "marked";
+
+import "./App.css";
+
 function App() {
-  return <div className="App"></div>;
+  const [preview, setPreview] = useState("");
+
+  const updatePreview = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setPreview(marked.parse(e.target.value));
+  };
+
+  return (
+    <div className="App">
+      <div className="input">
+        <textarea onChange={(e) => updatePreview(e)} />
+      </div>
+      <div className="preview">
+        <div dangerouslySetInnerHTML={{ __html: preview }} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
